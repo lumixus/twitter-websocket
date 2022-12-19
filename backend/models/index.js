1bfd7688ca1c7516b570f9d221d40cbc7c0b0a28
Tümer Altunbaş <tumeraltunbass@gmail.com>
2022-12-19 10:39:44 +0300

    retweet and bookmark relationships added

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,16 +2,14 @@ import Tweet from "./Tweet.js";
 import User from "./User.js"
 import Mention from "./Mention.js"
 import Favorite from "./Favorite.js";
-
-
+import Retweet from "./Retweet.js";
+import Bookmark from "./Bookmark.js";
 
 Tweet.belongsTo(User, {
     foreignKey: {
         allowNull:false
     }
 });
-
-
 
 Mention.belongsTo(Tweet, {
     foreignKey: {
@@ -46,8 +44,46 @@ Favorite.belongsToMany(Mention, {
     through: "FavoriteMention"
 });
 
+Retweet.belongsToMany(User, {
+        foreignKey:{
+            allowNull:false,
+        },
+        through: "RetweetUser"
+});
 
-export {Mention, Tweet, User, Favorite}
+Retweet.belongsToMany(Mention, {
+    foreignKey:{
+        allowNull:false,
+    },
+    through: "RetweetMention"
+});
 
+Retweet.belongsToMany(Tweet, {
+    foreignKey:{
+        allowNull:false,
+    },
+    through:"RetweetTweet"
+});
 
+Bookmark.belongsTo(User, {
+        foreignKey:{
+            allowNull:false
+        },
+        through:"BookmarkUser"
+});
 
+Bookmark.belongsTo(Tweet, {
+    foreignKey:{
+        allowNull:false
+    },
+    through:"BookmarkTweet"
+});
+
+Bookmark.belongsTo(Mention, {
+    foreignKey:{
+        allowNull:false
+    },
+    through:"BookmarkMention"
+});
+
+export {Mention, Tweet, User, Favorite, Retweet, Bookmark}
