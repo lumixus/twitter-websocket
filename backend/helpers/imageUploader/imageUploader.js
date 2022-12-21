041a52511fdc5409d01b9c62c349ee03751c443a
Tümer Altunbaş <tumeraltunbass@gmail.com>
2022-12-21 21:49:12 +0300

    video upload functionality

--- backend/helpers/imageUploader/imageUploader.js
+++ backend/helpers/imageUploader/imageUploader.js
@@ -7,7 +7,7 @@ export const imageUploader = (req, next)=>
     {
         const file = req.files.file //the name of file input = file
         let uploadPath = path.join(path.dirname("index.js"), "/public/uploads", file.name);
-        const allowedMimetypes = ["image/png", "image/jpg", "image/jpeg"];
+        const allowedMimetypes = ["image/png", "image/jpg", "image/jpeg","video/mp4"];
         if(!allowedMimetypes.includes(file.mimetype))
         return next(new CustomError(400, "Unsupported file type"));
         file.mv(uploadPath, function(err)
