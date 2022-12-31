12c6f118b3df96b634691100fc5f02cf590807d5
Tümer Altunbaş <tumeraltunbass@gmail.com>
2022-12-31 09:50:02 +0300

    homepage refactoring

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,4 +1,4 @@
-import { Op } from "sequelize";
+import { Op, Sequelize } from "sequelize";
 import Mention from "../models/Mention.js";
 import Tweet from "../models/Tweet.js";
 import User from "../models/User.js";
@@ -19,25 +19,19 @@ export const index = async (req, res, next) =>
 {
     try
     {
-        //too slow query
         const followingUsers = [];
-        // const response = [];
         const following = await Follow.findAll({attributes:["FollowerId"], where: {FollowingId:req.user.id}});
         for(var follow of following)
         {
             followingUsers.push(follow.FollowerId);
         }
-        // const tweets = await Tweet.findAll({attributes:["id", "content", "image", "createdAt", "UserId"], where: {UserId: {[Op.in]:followingUsers}}, order:sequelize.literal('createdAt desc'), limit:20});
-        // for(var tweet of tweets)
-        // {
-        //     const user = await User.findOne({attributes:["firstName", "lastName", "username"], where: {id:tweet.UserId}});
-        //     const favoriteCount = (await Favorite.findAll({where: {TweetId:tweet.id, isVisible:true}})).length
-        //     const obj = {"tweet":tweet, "user":user, "favoriteCount":favoriteCount};
-        //     response.push(obj);
-        // }
-        const response = await sequelize.query(`select * from Tweets left join Users on Tweets.UserId = Users.id left join Favorites on Tweets.Id = Favorites.TweetId where Tweets.UserID in (${followingUsers})`)
+        const response = await sequelize.query(`select 
+        Tweets.id, Tweets.content, Tweets.image, Tweets.favoriteCount, Tweets.mentionCount,
+        Users.username, Users.firstName, Users.lastName, Users.profilePicture
+        from Tweets 
+        left join Users on Tweets.UserId = Users.id 
+        where Tweets.UserID in (${followingUsers})`, { type: Sequelize.QueryTypes.SELECT })
         res.status(200).json({success:true, data:response});
-        // Tweets.id, Tweets.content, Tweets.image, Tweets.createdAt, Users.firstName, Users.lastName, Users.username, Users.profilePicture
     }
     catch(err)
     {
