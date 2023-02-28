87632d2be918398c37892666ef11a1e568d98937
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-28 19:53:33 +0300

    Folder refactoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { firstOnBoarding, verify,finalOnBoarding,login,logout,editProfile,profile,uploadPhoto,forgotPassword,resetPassword,changePassword,deactiveAccount,emailConfirmation,removePicture,follow,unfollow,bookmarks,favorites } from "../controllers/auth.js";
+import { firstOnBoarding, verify,finalOnBoarding,login,logout,forgotPassword,resetPassword,changePassword,deactiveAccount,emailConfirmation,follow,unfollow } from "../controllers/auth.js";
 import { isAuth } from "../middlewares/auth/auth.js";
 import { isUserExist } from "../middlewares/database/queryHelpers.js";
 
@@ -8,19 +8,13 @@ const router = express.Router();
 router.post("/firstonboarding", firstOnBoarding);
 router.post("/verify", verify);
 router.post("/finalonboarding" ,finalOnBoarding);
-router.post("/login", isUserExist, login);
+router.post("/login", login);
 router.get("/logout", isAuth, logout);
-router.put("/edit", isAuth, editProfile);
-router.post("/profile", [isAuth, isUserExist], profile);
-router.post("/upload", isAuth, uploadPhoto);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword", resetPassword);
 router.put("/passwordchange", isAuth, changePassword);
 router.post("/deactive", isAuth, deactiveAccount);
 router.get("/emailconfirmation", emailConfirmation);
-router.get("/removepicture", isAuth, removePicture);
 router.post("/follow", [isAuth,isUserExist], follow);
 router.post("/unfollow", [isAuth,isUserExist], unfollow);
-router.get("/bookmarks", isAuth, bookmarks);
-router.get("/favorites", isAuth, favorites);
 export default router;
