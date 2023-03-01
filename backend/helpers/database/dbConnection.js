21be8a9b3eb4dd496ecc2e0c531dcbb43685042b
lumixus <44450220+lumixus@users.noreply.github.com>
2023-03-01 20:03:55 +0300

    update static db credentials with variables

--- backend/helpers/database/dbConnection.js
+++ backend/helpers/database/dbConnection.js
@@ -1,12 +1,22 @@
 import {Sequelize} from "sequelize"; //Getting Sequelize class from sequelize package
+import dotenv from "dotenv"; //importing dotenv module. It gonna allow us to reach config's contents on process.env
 
-// const {DB_HOST, DB_USER, DB_PASS, DB_NAME} = process.env;
+dotenv.config({path:"./config/config.env"}); //specifying the path of config file
 
+let {DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT} = process.env;
 
-const sequelize = new Sequelize("twitter-websocket", "tumertw", "tumer", {
-    host: "92.205.8.227",
+if(process.env.NODE_ENV == 'development'){
+    DB_USER = process.env.LOCAL_DB_USER;
+    DB_PASS = process.env.LOCAL_DB_PASS;
+    DB_HOST = process.env.LOCAL_DB_HOST;
+    DB_NAME = process.env.LOCAL_DB_NAME;
+    DB_PORT = process.env.LOCAL_DB_PORT;
+}
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
     dialect: "mysql",
-    port:"3306"
+    port: DB_PORT
 });
 
 try
