8fdaf4fa7062f3018b9b2250f55cb06a5af87790
lumixus <44450220+lumixus@users.noreply.github.com>
2023-03-05 16:07:50 +0300

    Update default.png

--- frontend/src/Components/TweetCard/index.jsx
+++ frontend/src/Components/TweetCard/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './TweetCard.module.css'
 import moment from "moment"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEllipsis} from '@fortawesome/free-solid-svg-icons'
+import {faEllipsis, faComment, faRetweet, faHeart} from '@fortawesome/free-solid-svg-icons'
 
 
 const TweetCard = ({tweet}) => {
@@ -10,13 +10,13 @@ const TweetCard = ({tweet}) => {
     <>
     <div className={styles.tweetCard}>
         <div className={styles.profilePic}>
-            <img src="default_profile_normal.png" alt="" />
+            <img style={{maxHeight: "100%", maxWidth: "100%"}} src="http://localhost:8080/uploads/default.png" alt="" />
         </div>
         <div className={styles.rightSide}>
             <div className={styles.topSide}>
                 <div style={{display:"flex", flexDirection:"row"}}>
-                    <p style={{fontWeight:"bold"}}>{tweet.name}</p> 
-                    <span style={{marginLeft:"10px"}}>@{tweet.username}</span>
+                    <p style={{fontWeight:"bold"}}>{tweet["User.name"]}</p> 
+                    <span style={{marginLeft:"10px"}}>@{tweet["User.username"]}</span>
                     <span style={{marginLeft:"5px"}}>·</span>
                     <span style={{marginLeft:"5px"}}>{moment(tweet.createdAt).fromNow()}</span>
                 </div>
@@ -26,6 +26,22 @@ const TweetCard = ({tweet}) => {
             </div>
             <div>
                 {tweet.content}
+            </div>
+        </div>
+    </div>
+    <div className={styles.bottomSide}>
+        <div className={styles.tweetIcons}>
+            <div>
+                <FontAwesomeIcon icon={faComment} />
+                <div className={styles.iconValue}>{tweet.mentionCount}</div>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faRetweet} />
+                <div className={styles.iconValue}>{tweet.retweetCount}</div>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faHeart} />
+                <div className={styles.iconValue}>{tweet.favoriteCount}</div>
             </div>
         </div>
     </div>
