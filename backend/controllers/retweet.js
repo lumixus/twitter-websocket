8578547251f65d50accbb4f55f736b5a8d33aa29
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-09 12:30:25 +0300

    Update retweet endpoints

--- backend/controllers/retweet.js
+++ backend/controllers/retweet.js
@@ -1,17 +1,48 @@
 import Retweet from "../models/Retweet.js";
 import CustomError from "../helpers/error/CustomError.js";
+import Tweet from "../models/Tweet.js";
 
 export const reTweet = async(req, res, next) =>
 {
     try
     {
         const {tweet_id, content} = req.body;
+
         let {mention_id} = req.body;
-        if(mention_id === undefined)
-        mention_id = null;
-        if(await Retweet.count({where: {TweetId:tweet_id, MentionId:mention_id, UserId: req.user.id}}))
-        return next(new CustomError(400, "You already retweeted this"));
-        const reTweet = await Retweet.create({TweetId:tweet_id, MentionId:mention_id, UserId:req.user.id, content:content});
+        
+        if(mention_id === undefined){
+            mention_id = null;
+        }
+
+        const reTweetCheck = await Retweet.count({
+            where: {
+                TweetId:tweet_id, 
+                MentionId:mention_id, 
+                UserId: req.user.id
+            }
+        });
+
+        if(reTweetCheck){
+            return next(new CustomError(400, "You already retweeted this"));
+        }
+
+        const reTweet = await Retweet.create({
+            TweetId:tweet_id, 
+            MentionId:mention_id, 
+            UserId:req.user.id, 
+            content:content
+        });
+
+        const tweet = await Tweet.findOne({
+            where:{
+                id:tweet_id
+            },
+            attributes:["id", "retweetCount"]
+        });
+        
+        tweet.retweetCount += 1;
+        await tweet.save();
+
         res.status(200).json({success:true, data:reTweet});
     }
     catch(err)
@@ -43,6 +74,16 @@ export const undoReTweet = async (req, res, next) =>
             TweetId:tweet_id,
             MentionId: mention_id
         }});
+
+        const tweet = await Tweet.findOne({
+            where:{
+                id:tweet_id
+            },
+            attributes:["id", "retweetCount"]
+        });
+        
+        tweet.retweetCount -= 1;
+        await tweet.save();
         
         res.status(200).json({success:true, message:"Undo retweet successfull"});
     }
