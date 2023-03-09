b46a89cdaf3d9e12465b5ac5633b1a2a8041bdbc
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-09 12:29:22 +0300

    Add clean code rules

--- backend/controllers/mention.js
+++ backend/controllers/mention.js
@@ -8,7 +8,15 @@ export const getMentionsByTweet = async(req, res, next) =>
     try
     {
         const {tweet_id} = req.body;
-        const mentions = await Mention.findAll({where:{TweetId:tweet_id, isVisible:true}, attributes:["id","TweetId", "UserId" ,"content", "image"]});
+
+        const mentions = await Mention.findAll({
+            where:{
+                TweetId:tweet_id, 
+                isVisible:true
+            }, 
+            attributes: ["id","TweetId", "UserId" ,"content", "image"]
+        });
+        
         res.status(200).json({success:true, mentions: mentions});
     }
     catch(err)
@@ -22,12 +30,36 @@ export const createMention = async(req, res, next) =>
     try 
     {
         const {tweet_id, content} = req.body;
-        const tweet = await Tweet.findOne({attributes:['id', "mentionCount"], where: {id:tweet_id}})
+
+        const tweet = await Tweet.findOne({
+            attributes:['id', "mentionCount"], 
+            where: {
+                id:tweet_id
+            }
+        })
+        
         const fileName = imageUploader(req, next);
-        if(content == null && fileName == null)
-        return next(new CustomError(400, "Content or File must be provided"));
-        const mention = await Mention.create({content:content, image:fileName, TweetId: tweet_id, UserId:req.user.id});
-        await tweet.update({mentionCount: (await Mention.findAll({attributes:['id'], where:{TweetId:tweet.id, isVisible:true}})).length});
+
+        if(content == null && fileName == null){
+            return next(new CustomError(400, "Content or File must be provided"));
+        }
+        
+        const mention = await Mention.create({
+            content:content, 
+            image:fileName, 
+            TweetId: tweet_id, 
+            UserId:req.user.id
+        });
+        
+        await tweet.update({
+            mentionCount: (await Mention.findAll({
+                attributes:['id'], 
+                where:{
+                    TweetId:tweet.id, 
+                    isVisible:true
+                }}
+            )).length});
+        
         res.status(200).json({success:true, data:mention});
     }
     catch(err)
@@ -41,10 +73,31 @@ export const deleteMention = async(req, res, next) =>
     try
     {
         const {tweet_id,mention_id} = req.body;
-        const mention = await Mention.findOne({where:{id:mention_id}, attributes:["id", "isVisible", "hidByUser"]});
-        const tweet = await Tweet.findOne({attributes:['id','mentionCount'],where: {id:tweet_id}});
+
+        const mention = await Mention.findOne({
+            where:{
+                id:mention_id
+            }, 
+            attributes:["id", "isVisible", "hidByUser"]
+        });
+        const tweet = await Tweet.findOne({
+            attributes:['id','mentionCount'],
+            where: {
+                id:tweet_id
+            }
+        });
+        
         await mention.update({isVisible:false, hidByUser:true});
-        await tweet.update({mentionCount: (await Mention.findAll({attributes:['id'], where:{TweetId:tweet.id, isVisible:true}})).length});
+
+        await tweet.update({
+            mentionCount: (await Mention.findAll({
+                attributes:['id'], 
+                where:{
+                    TweetId:tweet.id, 
+                    isVisible:true
+                }}
+            )).length});
+        
         res.status(200).json({success:true, message: "Your mention has been deleted"});
     }
     catch(err)
